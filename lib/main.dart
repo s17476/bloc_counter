ba584246af88f38fa07a1b27014bcd277045af54
s17476 <s17476@pjwstk.edu.pl>
2022-03-17 10:44:14 +0100

    BlocProvider value -> create fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:bloc_counter/logic/cubit/counter_cubit.dart';
 import 'package:bloc_counter/presentation/router/app_router.dart';
-import 'package:bloc_counter/presentation/screens/home_screen.dart';
-import 'package:bloc_counter/presentation/screens/second_screen.dart';
-import 'package:bloc_counter/presentation/screens/third_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -17,8 +14,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider.value(
-      value: CounterCubit(),
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
